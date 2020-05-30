403fca85fcd16cb52656e4fb579e1d09c42bae8e
Jaga <Jaga@DESKTOP-UHCTFMH>
2020-05-30 16:39:19 -0400

    broken but with cleanup

--- OpenCV/FaceShift.cpp
+++ OpenCV/FaceShift.cpp
@@ -1,3 +1,11 @@
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
@@ -6,18 +14,24 @@
 #include <thread>
 #include <winsock2.h>
 #include <Ws2tcpip.h>
+#include <stdio.h>
 
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
 
-using namespace std;
+//using namespace std;
 using namespace cv;
 void detectAndDisplay(Mat frame);
+int my_s();
 CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
 Rect roi;
 
 auto tp1 = std::chrono::system_clock::now();
 auto tp2 = std::chrono::system_clock::now();
+
+
+SOCKET ListenSocket = INVALID_SOCKET;
+SOCKET ClientSocket = INVALID_SOCKET;
 
 int main(int argc, const char** argv)
 {
@@ -34,69 +48,28 @@ int main(int argc, const char** argv)
     //-- 1. Load the cascades
     if (!face_cascade.load(face_cascade_name))
     {
-        cout << "--(!)Error loading face cascade\n";
+        std::cout << "--(!)Error loading face cascade\n";
         return -1;
     };
     if (!eyes_cascade.load(eyes_cascade_name))
     {
-        cout << "--(!)Error loading eyes cascade\n";
+        std::cout << "--(!)Error loading eyes cascade\n";
         return -1;
     };
     int camera_device = parser.get<int>("camera");
 
-    
+// Run socket server 
 
-    WSADATA wsa;
-    SOCKET s;
-    sockaddr_in server;
-    int iResult;
+    std::thread(my_s).detach();
+   
 
-    cout << "Initializing WInsock..." << endl;
-
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        cout << "Failed. Error Code :" << WSAGetLastError() << endl;
-        return 1;
-        }
-     
-    cout << "Ininialized." << endl;
-
-    if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
-        cout << "Could not create socket :" <<  WSAGetLastError() << endl;
-
-    cout << "Socket created." << endl;
-
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_family = AF_INET;
-    server.sin_port = htons(2345);
-
-
-
-    iResult = bind(s, (SOCKADDR *)&server, sizeof(server));
-    if (iResult == SOCKET_ERROR)
-    {
-        puts("Bind failed");
-        return 1;
-    }
-    else
-        cout << "Bind Connected" << endl;;
-
-
-    // Connect to localhost
-
-    //if (connect(s, (struct sockaddr*)&server, sizeof(server)) < 0) {
-    //    puts("Conncet errror");
-    //    return 1;
-
-    //}
-
-    //puts("Connected");
 
     VideoCapture capture;
     //-- 2. Read the video stream
     capture.open(camera_device);
     if (!capture.isOpened())
     {
-        cout << "--(!)Error opening video capture\n";
+        std::cout << "--(!)Error opening video capture\n";
         return -1;
     }
     Mat frame;
@@ -104,10 +77,13 @@ int main(int argc, const char** argv)
     Mat z_frame, z_frame2;
     roi = Rect(0, 0, frame.cols, frame.rows);
     Rect cur_roi(0, 0, frame.cols, frame.rows);
-    float v = 10;
+    float v = 30;
     float d=0;
+    float dx_a, dy_a, dw_a;
+    bool zooming = false;
+    Rect w_roi;
         while (capture.read(frame))
-    {
+        {
         // Handle Timing
         tp2 = std::chrono::system_clock::now();
         std::chrono::duration<float> elapsedTime = tp2 - tp1;
@@ -116,7 +92,7 @@ int main(int argc, const char** argv)
             
         if (frame.empty())
         {
-            cout << "--(!) No captured frame -- Break!\n";
+            std::cout << "--(!) No captured frame -- Break!\n";
             break;
         }
         //-- 3. Apply the classifier to the frame
@@ -125,53 +101,85 @@ int main(int argc, const char** argv)
         
         if (i % 90 == 0)
             std::thread(detectAndDisplay,frame).detach(); 
-        d = ceil(v * fElapsedTime);
-        int dx(abs(cur_roi.x - roi.x));
+       
+        float dx(abs(cur_roi.x - roi.x));
+        float dy(abs(cur_roi.y - roi.y));
+        float D = sqrt((dx * dx) + (dy * dy));
+        
+ 
+        if (D > 120 && !zooming) {
+            w_roi = roi;
+            zooming = true;
+            dx_a = cur_roi.x;
+            dy_a = cur_roi.y;
+            dw_a = cur_roi.width;
+        }
+        
+        if (!w_roi.empty() && !cur_roi.empty()) {
+            if (zooming) {
+                d =  (v * fElapsedTime);
 
-        if (!roi.empty() && !cur_roi.empty()){
+                if (dx_a - w_roi.x > 0)
+                    dx_a = dx_a - d;
+                else if (dx_a - w_roi.x < 0)
+                    dx_a = dx_a + d;
+                if (dx_a < 0)
+                    dx_a = 0;
+                if (dx_a > frame.cols)
+                    dx_a = frame.cols;
+
+                if (dy_a - w_roi.y > 0)
+                    dy_a = dy_a - d;
+                else if (dy_a - w_roi.y < 0)
+                    dy_a = dy_a + d;
+
+                if (dy_a < 0)
+                    dy_a = 0;
+                if (dy_a > frame.rows)
+                    dy_a = frame.rows;
+
+                if (dw_a - w_roi.width > 0)
+                    dw_a = dw_a - d;
+                else if (dw_a - w_roi.width < 0)
+                    dw_a = dw_a + d;
+                if (dw_a < 0)
+                    dw_a = 0;
+                if (dw_a > frame.cols)
+                    dw_a = frame.cols;
+                
+            cur_roi.x = dx_a;
+            cur_roi.y = dy_a;
+            cur_roi.width = dw_a;
+            }
+            /*  if (cur_roi.height - roi.height > 0)
+                  cur_roi.height = cur_roi.height - d;
+              else if (cur_roi.height - roi.height < 0)
+                  cur_roi.height = cur_roi.height + d;
+              if (cur_roi.height < 0)
+                  cur_roi.height = 0;
+              if (cur_roi.height > frame.rows)
+                  cur_roi.height = frame.rows;*/
             
-            if (cur_roi.x - roi.x > 0)
-                cur_roi.x = cur_roi.x - d;
-            else if (cur_roi.x - roi.x < 0)
-                cur_roi.x = cur_roi.x + d;
-            if (cur_roi.x < 0)
-                cur_roi.x = 0;
-            if (cur_roi.x > frame.cols)
-                cur_roi.x = frame.cols;
 
-            if (cur_roi.y - roi.y > 0)
-                cur_roi.y = cur_roi.y - d;
-            else if (cur_roi.y - roi.y < 0)
-                cur_roi.y = cur_roi.y + d;
-            if (cur_roi.y < 0)
-                cur_roi.y = 0;
-            if (cur_roi.y > frame.rows)
-                cur_roi.y = frame.rows;
-            
-            if (cur_roi.width - roi.width > 0)
-                cur_roi.width = cur_roi.width - d;
-            else if (cur_roi.width - roi.width < 0)
-                cur_roi.width = cur_roi.width + d;
-            if (cur_roi.width < 0)
-                cur_roi.width = 0;
-            if (cur_roi.width > frame.cols)
-                cur_roi.width = frame.cols;
 
-            if (cur_roi.height - roi.height > 0)
-                cur_roi.height = cur_roi.height - d;
-            else if (cur_roi.height - roi.height < 0)
-                cur_roi.height = cur_roi.height + d;
-            if (cur_roi.height < 0)
-                cur_roi.height = 0;
-            if (cur_roi.height > frame.rows)
-                cur_roi.height = frame.rows;
 
-            d = 0;
-
+            cur_roi.height = floor(float(cur_roi.width) * (float(frame.rows) / float(frame.cols)));
+            if (cur_roi.height + cur_roi.y > frame.rows)
+                cur_roi.height = frame.rows - cur_roi.y;
+                                      
             z_frame = frame(cur_roi);
             resize(z_frame, z_frame, frame.size(), 0, 0, INTER_LINEAR);
             imshow("Capture - Face detection", z_frame);
-            
+            z_frame = (z_frame.reshape(0, 1)); // to make it continuous           
+            int  imgSize = z_frame.total() * z_frame.elemSize();
+            if (ClientSocket != INVALID_SOCKET)
+                int bytes = send(ClientSocket, reinterpret_cast<const char*>(z_frame.data), imgSize, 0);
+            if (cur_roi.x == roi.x && cur_roi.y == roi.y) {
+                zooming = false;
+                d = 0;
+            }
+
+
         }
         if (waitKey(10) == 27)
         {
@@ -210,7 +218,8 @@ void detectAndDisplay(Mat frame)
     }
     else{
         int new_w(3 * faces[0].width);
-        int new_h(3 * faces[0].height);
+        int new_h(floor(float(new_w) * (float(frame.rows) / float(frame.cols))));
+    /*    int new_h(3 * faces[0].height);*/
         if (new_w > frame.cols)
             new_w = frame.cols;
         if (new_h > frame.rows)
@@ -228,8 +237,73 @@ void detectAndDisplay(Mat frame)
         //-- Show what you got    
         roi= Rect(crop_p.x, crop_p.y, new_w, new_h);
         Mat z_frame= frame(roi);
-       /* resize(z_frame, z_frame, frame.size(), 0,0, INTER_LINEAR);
-        imshow("Capture - Face detection", z_frame);*/
-    //    return roi;
+    }
+}
+int my_s() {
+
+    // Declare some variables
+    WSADATA wsaData;
+
+    int iResult = 0;            // used to return function results
+
+    //// the listening socket to be created
+    //SOCKET ListenSocket = INVALID_SOCKET;
+    //SOCKET ClientSocket = INVALID_SOCKET;
+
+    // The socket address to be passed to bind
+    sockaddr_in service;
+
+    //----------------------
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != NO_ERROR) {
+        wprintf(L"Error at WSAStartup()\n");
+        return 1;
+    }
+    //----------------------
+    // Create a SOCKET for listening for 
+    // incoming connection requests
+    ListenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (ListenSocket == INVALID_SOCKET) {
+        wprintf(L"socket function failed with error: %u\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+    //----------------------
+    // The sockaddr_in structure specifies the address family,
+    // IP address, and port for the socket that is being bound.
+    service.sin_family = AF_INET;
+    service.sin_addr.s_addr = inet_addr("127.0.0.1");
+    service.sin_port = htons(2345);
+
+    //----------------------
+    // Bind the socket.
+    iResult = bind(ListenSocket, (SOCKADDR*)&service, sizeof(service));
+    if (iResult == SOCKET_ERROR) {
+        wprintf(L"bind failed with error %u\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+    else
+        wprintf(L"bind returned success\n");
+
+
+    iResult = listen(ListenSocket, SOMAXCONN);
+    if (iResult == SOCKET_ERROR) {
+        printf("listen failed with error: %d\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+    // Accept a client socket
+    while (true) {
+        ClientSocket = accept(ListenSocket, NULL, NULL);
+        if (ClientSocket == INVALID_SOCKET) {
+            printf("accept failed with error: %d\n", WSAGetLastError());
+            closesocket(ListenSocket);
+            WSACleanup();
+            //return 1;
+        }
     }
 }
