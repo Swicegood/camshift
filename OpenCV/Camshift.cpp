c82c909c1829c1987e12caaf55b94697e3fb4481
Jaga <Jaga@DESKTOP-UHCTFMH>
2020-06-08 18:11:19 -0400

    Cleaned up and changed name

--- OpenCV/Camshift.cpp
+++ OpenCV/Camshift.cpp
@@ -1,107 +1,228 @@
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
 #include <iostream>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/video.hpp>
+#include <thread>
+#include <stdio.h>
+
+
+//using namespace std;
 using namespace cv;
-using namespace std;
-int main()
+void detectAndDisplay(Mat frame);
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
+Rect roi;
+
+auto tp1 = std::chrono::system_clock::now();
+auto tp2 = std::chrono::system_clock::now();
+
+
+
+bool done = true;
+
+int main(int argc, const char** argv)
 {
-    VideoCapture cam;
-    while (!cam.open(0))cerr << "failed to open cam" << endl;
-    Mat frame, roi, hsv_roi, mask;
-    // take first frame of the video
-    cam >> frame;
-    // setup initial location of window
-    Rect track_window(300, 200, 100, 50); // simply hardcoded the values
-    // set up the ROI for tracking
-    roi = frame(track_window);
-    cvtColor(roi, hsv_roi, COLOR_BGR2HSV);
-    inRange(hsv_roi, Scalar(0, 60, 32), Scalar(180, 255, 255), mask);
-    float range_[] = { 0, 180 };
-    const float* range[] = { range_ };
-    Mat roi_hist;
-    int histSize[] = { 180 };
-    int channels[] = { 0 };
-    calcHist(&hsv_roi, 1, channels, mask, roi_hist, 1, histSize, range);
-    normalize(roi_hist, roi_hist, 0, 255, NORM_MINMAX);
-    // Setup the termination criteria, either 10 iteration or move by atleast 1 pt
-    TermCriteria term_crit(TermCriteria::EPS | TermCriteria::COUNT, 10, 1);
-    while (true) {
-        Mat hsv, dst;
-        cam >> frame;
+    
+    CommandLineParser parser(argc, argv,
+        "{help h||}"
+        "{face_cascade|data/haarcascades/haarcascade_frontalface_alt.xml|Path to face cascade.}"
+        "{eyes_cascade|data/haarcascades/haarcascade_eye_tree_eyeglasses.xml|Path to eyes cascade.}"
+        "{camera|0|Camera device number.}");
+    parser.about("\nThis program demonstrates using the cv::CascadeClassifier class to detect objects (Face + eyes) in a video stream.\n"
+        "You can use Haar or LBP features.\n\n");
+    parser.printMessage();
+    String face_cascade_name = samples::findFile(parser.get<String>("face_cascade"));
+    String eyes_cascade_name = samples::findFile(parser.get<String>("eyes_cascade"));
+    //-- 1. Load the cascades
+    if (!face_cascade.load(face_cascade_name))
+    {
+        std::cout << "--(!)Error loading face cascade\n";
+        return -1;
+    };
+    if (!eyes_cascade.load(eyes_cascade_name))
+    {
+        std::cout << "--(!)Error loading eyes cascade\n";
+        return -1;
+    };
+    int camera_device = parser.get<int>("camera");
+
+    //Control for speed
+    int v = 165;
+    namedWindow("Capture - Face detection");
+    createTrackbar("Speed", "Capture - Face detection", &v, 254, NULL);
+
+   
+    VideoCapture capture;
+    //-- 2. Read the video stream
+    capture.open(camera_device);
+    if (!capture.isOpened())
+    {
+        std::cout << "--(!)Error opening video capture\n";
+        return -1;
+    }
+    VideoWriter writer(
+        "appsrc ! videoconvert ! video/x-raw,format=YUY2,width=640,height=480,framerate=30/1 ! jpegenc ! rtpjpegpay ! udpsink host=127.0.0.1 port=5000",
+        CAP_GSTREAMER,
+        0, // four cc
+        30, //fps
+        Size(640, 480),
+        true); //isColor
+    if (!writer.isOpened()) {
+        std::cerr << "VideoWriter not opened" << std::endl;
+        exit(-1);
+    }
+    Mat frame;
+    int i = 0; 
+    Mat z_frame, z_frame2;
+    roi = Rect(0, 0, frame.cols, frame.rows);
+    Rect cur_roi(0, 0, frame.cols, frame.rows);
+    float dx,dy,dw,D=dx=dy=dw=0;
+    bool zoom_reached = true;
+    Rect target_roi;
+    float ar = (float(frame.rows) / float(frame.cols));
+        while (capture.read(frame))
+        {
+        //Apsect ratio
+        float ar = (float(frame.rows) / float(frame.cols));
+        // Handle Timing
+        tp2 = std::chrono::system_clock::now();
+        std::chrono::duration<float> elapsedTime = tp2 - tp1;
+        tp1 = tp2;
+        float fElapsedTime = elapsedTime.count();
+            
         if (frame.empty())
+        {
+            std::cout << "--(!) No captured frame -- Break!\n";
             break;
-        cvtColor(frame, hsv, COLOR_BGR2HSV);
-        calcBackProject(&hsv, 1, channels, roi_hist, dst, range);
-        // apply camshift to get the new location
-        RotatedRect rot_rect = CamShift(dst, track_window, term_crit);
-        // Draw it on image
-        Point2f points[4];
-        rot_rect.points(points);
-        for (int i = 0; i < 4; i++)
-            line(frame, points[i], points[(i + 1) % 4], 255, 2);
-       /* Mat roi_final;
-        roi_final = frame;
-        bool fullscreen(false);
-        float width(points[3].x - points[0].x);
-        float height(points[2].y - points[1].y);
-        if (width < 0 || height < 0 || width > frame.cols || height > frame.cols)
-            fullscreen = true;
-        Point2f new_p[4];
-        for (int i = 0; i < 4; i++) {
-            if ((points[i].x < 0) || (points[i].y < 0) || (points[i].x > frame.cols) || (points[i].y > frame.rows)) {
-                fullscreen = true;
-                break;
+        }
+        //-- 3. Apply the classifier to the frame
+        if (i == 0)
+            cur_roi = Rect(0, 0, frame.cols, frame.rows);
+
+
+        if (done && zoom_reached) {  //face detected and roi created. Ready to zoom in
+            target_roi = roi;
+            //Start panning and zooming gradually frame by frame
+            dx=(target_roi.x - cur_roi.x);
+            dy=(target_roi.y - cur_roi.y);
+            dw = (target_roi.width - cur_roi.width); //Change in width
+            D = sqrt((dx * dx) + (dy * dy));
+            if (((D < 90) && (dw < 60)) || v ==0) {
+                target_roi = cur_roi;
             }
 
+            done = false;
         }
-        for (int i = 0; i < 4; i++) {
-            switch (i)
-            {
-            case 0:
-                new_p[i].x = (points[i].x - width);
-                new_p[i].y = (points[i].y - width);
-                break;
-            case 1:
-                new_p[i].x = (points[i].x + width);
-                new_p[i].y = (points[i].y - width);
-                break;
-            case 2:
-                new_p[i].x = (points[i].x - width);
-                new_p[i].y = (points[i].y + width);
-                break;
-            case 3:
-                new_p[i].x = (points[i].x - width);
-                new_p[i].y = (points[i].y + width);
-                break;
-           
-            }
-            if (new_p[i].x < 0)
-                new_p[i].x = 0;
-            if (new_p[i].x > frame.cols)
-                new_p[i].x = frame.cols;
-            if (new_p[i].y < 0)
-                new_p[i].y = 0;
-            if (new_p[i].y > frame.rows)
-                new_p[i].y = frame.rows;
 
-        }
+        if (i % 90 == 0)   // Run face detection code only occasionally to save CPU cycles
+            std::thread(detectAndDisplay,frame).detach(); 
        
-        if (((new_p[3].x - new_p[0].x) > frame.cols) || ((new_p[2].y - new_p[1].y) > frame.rows)) 
-            fullscreen = true;
-        int new_w(new_p[3].x - new_p[0].x);
-        int new_h(new_p[2].y - new_p[1].y);
-        if (((new_p[0].x + new_w) > frame.cols) || ((new_p[0].y + new_h) > frame.rows))
-            fullscreen = true;
-        if (!fullscreen) {
-            Rect window(new_p[0].x, new_p[0].y, new_w, new_h);
-            roi_final = frame(window);
-        }*/
-        imshow("img2", frame);
-         int keyboard = waitKey(30);
-        if (keyboard == 'q' || keyboard == 27)
-            break;
+        dx = (target_roi.x - cur_roi.x);
+        dy = (target_roi.y - cur_roi.y);
+        dw = (target_roi.width - cur_roi.width); //Change in width
+        
+        
+        if (!target_roi.empty() && !cur_roi.empty()) {
+
+
+            cur_roi.x = (dx / (255-v)) >= 0 ? (cur_roi.x) + ceil(dx / (255-v)) : (cur_roi.x) + floor(dx / (255-v));
+            if (cur_roi.x < 0)
+                cur_roi.x = 0;
+            if (cur_roi.x > frame.cols)
+                cur_roi.x = frame.cols;
+
+            cur_roi.y = (dy / (255-v)) >= 0? (cur_roi.y) + ceil(dy / (255-v)) : (cur_roi.y) + floor(dy / (255-v));
+            if (cur_roi.y < 0)
+                cur_roi.y = 0;
+            if (cur_roi.y > frame.rows)
+                cur_roi.y = frame.rows;
+
+            cur_roi.width = (dw / (255-v)) >= 0 ? ((cur_roi.width) + ceil(dw / (255-v))) : ((cur_roi.width) + floor(dw / (255-v)));
+            if (cur_roi.width < 0)
+                cur_roi.width = 0;
+            if (cur_roi.width > frame.cols)
+                cur_roi.width = frame.cols;
+
+            cur_roi.height = floor(float(cur_roi.width) * ar);
+            if (cur_roi.height + cur_roi.y > frame.rows)
+                cur_roi.y = frame.rows - cur_roi.height;
+                                      
+            z_frame = frame(cur_roi);  // Zoomed frame
+            resize(z_frame, z_frame, frame.size(), 0, 0, INTER_LINEAR); //Fill whole "window"
+            imshow("Capture - Face detection", z_frame);
+            z_frame = (z_frame.reshape(0, 1)); // to make it continuous     
+            writer.write(z_frame);           // Write fame to Gstreamer pipeline
+            if ((cur_roi.x == target_roi.x) && (cur_roi.y == target_roi.y))  {
+                zoom_reached = true;
+            }
+
+
+        }
+        if (waitKey(10) == 27)
+        {
+            break; // escape
+        }
+        i++;
+    }
+    return 0;
+}
+void detectAndDisplay(Mat frame)
+{
+    Mat frame_gray;
+    cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+    equalizeHist(frame_gray, frame_gray);
+    //-- Detect faces
+    std::vector<Rect> faces;
+    face_cascade.detectMultiScale(frame_gray, faces);
+    //for (size_t i = 0; i < faces.size(); i++)
+    //{
+    //    Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
+    //    ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4);
+    //    Mat faceROI = frame_gray(faces[i]);
+    //    //-- In each face, detect eyes
+    //    std::vector<Rect> eyes;
+    //    eyes_cascade.detectMultiScale(faceROI, eyes);
+    //    for (size_t j = 0; j < eyes.size(); j++)
+    //    {
+    //        Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
+    //        int radius = cvRound((eyes[j].width + eyes[j].height) * 0.25);
+    //        circle(frame, eye_center, radius, Scalar(255, 0, 0), 4);
+    //    }
+    //}
+    if (faces.size() == 0 || faces[0].x < 0 || faces[0].y < 0 || (faces[0].x + faces[0].width > frame.cols) || (faces[0].y + faces[0].height > frame.rows)) {
+
+          roi = Rect(0, 0, frame.cols, frame.rows);          
+    }
+    else{
+        int new_w(3 * faces[0].width);
+        int new_h(floor(float(new_w) * (float(frame.rows) / float(frame.cols))));
+
+        if (new_w > frame.cols)
+            new_w = frame.cols;
+        if (new_h > frame.rows)
+            new_h = frame.rows;
+        Point _center(faces[0].x + faces[0].width / 2, faces[0].y + faces[0].height / 2);
+        Point crop_p(_center.x - .5 * new_w, _center.y -  .5 * new_h);
+        if (crop_p.x + new_w > frame.cols)
+            crop_p.x = frame.cols - new_w;
+        else if (crop_p.x < 0)
+            crop_p.x = 0;
+        if (crop_p.y + new_h > frame.rows)
+            crop_p.y = frame.rows - new_h;
+        else if (crop_p.y < 0)
+            crop_p.y = 0;
+        //-- Show what you got    
+        roi= Rect(crop_p.x, crop_p.y, new_w, new_h);
+        Mat z_frame= frame(roi);
+        done = true;
     }
 }
